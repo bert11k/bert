68b29c5e6cc0e747673595a7966cd20b6b3e7cc5
bert11k <bert11k@yandex.ru>
2021-02-28 20:27:24 +0300

    Deal

---
+++ src/components/Dealpred.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="dealtovar">
+<h2>Предмет договора</h2>
+<p>1. Нефтяные масла</p>
+<p>2. Бензин</p>
+<p>3. Солярка</p>
+<p>4. Керосин</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Dealpred",
+};
+</script>
+<style lang="scss" scoped>
+.dealtovar{
+background-color:#C4C4C4;
+width: 70%;
+margin-left: 50px;
+margin-top: 50px;
+padding: 15px;
+h2{
+  font-size: 1.6em;
+}
+p{
+  font-size: 1.4em;
+}
+}
+
+
+</style>
---
+++ src/views/Deal.vue
@@ -0,0 +1,28 @@
+<template>
+  <layout-main>
+    <div class="deal">
+    <Dealitem/>
+    <Dealpred/>
+        
+  
+    </div>
+  </layout-main>
+</template>
+
+<script>
+import LayoutMain from "../layouts/LayoutMain.vue";
+import Dealitem from "../components/Dealitem.vue";
+import Dealpred from "../components/Dealpred.vue";
+
+export default {
+  name: "Deal",
+  components: { LayoutMain, Dealitem, Dealpred},
+};
+</script>
+<style lang="scss" scoped>
+.deal{
+  padding-bottom: 50px;
+  margin-top: 10px;
+  background-color:white;
+}
+</style>
